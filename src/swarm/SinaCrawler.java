8b56e25bd4f0b7f05d94a263f7c4ac53e425d152
Steve Wang <iswangheng@gmail.com>
2011-10-13 21:20:14 +0800

    oops, commit again

--- src/swarm/SinaCrawler.java
+++ src/swarm/SinaCrawler.java
@@ -49,8 +49,8 @@ public class SinaCrawler  implements ActionListener
 		              }	
 		         );
 			
-			//System.setOut(printStream);
-			//System.setErr(printStream);
+			System.setOut(printStream);
+			System.setErr(printStream);
 	    }
 
 	    private void setUpUIComponent(final JFrame f)
